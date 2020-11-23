3ae198da3567b152a5637086a160cd8d241ad6af
confusdcodr <confusdcodr@users.noreply.github.com>
2020-11-23 18:01:12 -0500

    cleanup original stack and add lambda function

--- cdkworkshop/cdkworkshop_stack.py
+++ cdkworkshop/cdkworkshop_stack.py
@@ -1,9 +1,8 @@
+import os
+
 from aws_cdk import (
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-    core
+    core,
+    aws_lambda as _lambda
 )
 
 
@@ -12,13 +11,12 @@ class CdkworkshopStack(core.Stack):
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkworkshopQueue",
-            visibility_timeout=core.Duration.seconds(300),
+        my_lambda = _lambda.Function(
+            self, 
+            "HelloHandler", 
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset(os.path.join(os.getcwd(), "lambda")),
+            handler="hello.handler"
         )
 
-        topic = sns.Topic(
-            self, "CdkworkshopTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        
